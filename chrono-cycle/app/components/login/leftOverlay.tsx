f4abacda9772f6ad44fe775d8cd70cfcef404650
keith <keithneo00@gmail.com>
2025-02-08 01:01:04 +0800

    refactor(leftOverlay, RightOverlay): change text color to text-gray-200 instead of text-gray-400

--- chrono-cycle/app/components/login/leftOverlay.tsx
+++ chrono-cycle/app/components/login/leftOverlay.tsx
@@ -12,7 +12,7 @@ const LeftOverlay: React.FC<LeftOverlayProps> = ({ isAnimated, setAnimate }) => 
             <h1 className="w-4/5 text-5xl text-palette3 font-bold text-center">Welcome to ChronoCycle!</h1>
 
             <div className="w-full flex flex-col items-center gap-3">
-                <h3 className="text-lg text-gray-400">Already have an account?</h3>
+                <h3 className="text-lg text-gray-200">Already have an account?</h3>
                 <button className="w-1/5 p-1 rounded-xl border-palette3 border text-palette3 transition-colors duration-400 hover:bg-[#6e5a42] hover:border-[#b1b1b1] hover:text-[#b1b1b1]"
                 onClick={() => {
                     setAnimate(!isAnimated)
--- chrono-cycle/app/components/login/rightOverlay.tsx
+++ chrono-cycle/app/components/login/rightOverlay.tsx
@@ -12,7 +12,7 @@ const RightOverlay: React.FC<RightOverlayProps> = ({ isAnimated, setAnimate}) =>
             <h1 className="w-4/5 text-5xl text-palette3 font-bold text-center">Welcome back to ChronoCycle!</h1>
 
             <div className="w-full flex flex-col items-center gap-3">
-                <h3 className="text-lg text-gray-400">Don&apos;t have an account?</h3>
+                <h3 className="text-lg text-gray-200">Don&apos;t have an account?</h3>
                 <button className="w-1/5 p-1 rounded-xl border-palette3 border text-palette3 transition-colors duration-400 hover:bg-[#6e5a42] hover:border-[#b1b1b1] hover:text-[#b1b1b1]"
                 onClick={() => {
                     setAnimate(!isAnimated)
