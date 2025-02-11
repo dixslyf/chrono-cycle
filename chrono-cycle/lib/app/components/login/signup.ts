9874b08acc67e66eeddfa981c7ed15656f82f1fd
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2025-02-11 15:31:36 +0800

    refactor(signup, auth): disallow whitespace in passwords

--- chrono-cycle/lib/app/components/login/signup.ts
+++ chrono-cycle/lib/app/components/login/signup.ts
@@ -15,6 +15,7 @@ export const passwordSchema = z
     .string()
     .min(8, "Password must be at least 8 characters long")
     .max(128, "Password cannot exceed 128 characters")
+    .regex(/^\S*$/, "Password must not contain whitespace")
     .regex(/.*[A-Z]/, "Password must contain at least one uppercase letter")
     .regex(/.*[a-z]/, "Password must contain at least one lowercase letter")
     .regex(/.*[0-9]/, "Password must contain at least one number");
