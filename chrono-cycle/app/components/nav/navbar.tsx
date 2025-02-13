1e914331beda75725e35a5ddeeaa0c50d3ddf31f
keith <keithneo00@gmail.com>
2025-02-14 00:21:26 +0800

    feat(assets/): Add back the logo used in the readme

--- chrono-cycle/app/components/nav/navbar.tsx
+++ chrono-cycle/app/components/nav/navbar.tsx
@@ -13,7 +13,11 @@ const Navbar = () => {
                 {/* Hamburger menu & Logo */}
                 <div className="flex items-center gap-3">
                     {/* hamburger menu here */}
-
+                    <div className="grid justify-items-center gap-1.5">
+                        <span className="h-1 w-8 rounded-full bg-black"></span>
+                        <span className="h-1 w-8 rounded-full bg-black"></span>
+                        <span className="h-1 w-8 rounded-full bg-black"></span>
+                    </div>
                     {/* logo here */}
                     <Logo className=" text-palette3 h-14 w-auto"/>
                 </div>
