3318293bb7696e1b97a6bf4f914458c5a1c53eae
keith <keithneo00@gmail.com>
2025-02-14 18:14:15 +0800

    refactor(navbar): replace sidebar section with sidebar component
    feat(sidebar): Created a new sidebar component for the sidebar.

--- chrono-cycle/app/components/nav/navbar.tsx
+++ chrono-cycle/app/components/nav/navbar.tsx
@@ -2,25 +2,15 @@
 "use client";
 
 import { useState } from "react";
-import { usePathname } from "next/navigation";
 import IconLogo from "./topIcon";
-import {
-    Bell,
-    User,
-    House,
-    LayoutTemplate,
-    CircleHelp,
-    Cog,
-} from "lucide-react";
-import Link from "next/link";
+import { Bell, User } from "lucide-react";
+import Sidebar from "./sidebar";
 
 const Navbar = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false); // handles sidebar
     const [isNotiOpen, setIsNotiOpen] = useState<boolean>(false); // handles notification dropdown
     const [notifications, setNotifications] = useState<number>(3); // eslint-disable-line @typescript-eslint/no-unused-vars
     const [isUserOpen, setIsUserOpen] = useState<boolean>(false);
-
-    const pathname = usePathname();
 
     const toggleSidebar = () => {
         setIsSidebarOpen(!isSidebarOpen);
@@ -64,99 +54,12 @@ const Navbar = () => {
                     </button>
                 </div>
             </nav>
-            {/* side navbar */}
-            <nav
-                className={`fixed top-0 left-0 h-full w-[20%] p-4 bg-palette1 text-palette3 transform transition-transform duration-300 ease-in-out ${
-                    isSidebarOpen ? "translate-x-0 z-20" : "-translate-x-full"
-                }`}
-            >
-                {/* logo and close button */}
-                <div
-                    onClick={toggleSidebar}
-                    className="flex flex-row-reverse justify-between"
-                >
-                    <IconLogo isOpen={isSidebarOpen} />
-                </div>
-                {/* Navigation links */}
-                <div className="w-full mt-10 p-5 flex flex-col gap-8">
-                    {/* dashboard */}
-                    <div>
-                        <Link
-                            href="/dashboard"
-                            className={`flex gap-5 p-3 rounded-lg text-palette3 ${
-                                pathname == "/dashboard"
-                                    ? "bg-[#2b2a28]"
-                                    : "hover:bg-palette4"
-                            }`}
-                        >
-                            <House
-                                className="w-10 h-10"
-                                style={{ fill: "none" }}
-                            />
-                            <span className="font-semibold text-2xl">
-                                Dashboard
-                            </span>
-                        </Link>
-                    </div>
-                    {/* template */}
-                    <div>
-                        <Link
-                            href="/templates"
-                            className={`flex gap-5 p-3 rounded-lg text-palette3 ${
-                                pathname == "/template"
-                                    ? "bg-palette4"
-                                    : "hover:bg-palette4"
-                            }`}
-                        >
-                            <LayoutTemplate className="w-10 h-10" />
-                            <span className="font-semibold text-2xl">
-                                Template
-                            </span>
-                        </Link>
-                    </div>
-                    {/* circle help */}
-                    <div>
-                        <Link
-                            href="/help"
-                            className={`flex gap-5 p-3 rounded-lg text-palette3 ${
-                                pathname == "/help"
-                                    ? "bg-palette4"
-                                    : "hover:bg-palette4"
-                            }`}
-                        >
-                            <CircleHelp className="w-10 h-10" />
-                            <span className="font-semibold text-2xl">Help</span>
-                        </Link>
-                    </div>
-                    {/* settings */}
-                    <div>
-                        <Link
-                            href="/settings"
-                            className={`flex gap-5 p-3 rounded-lg text-palette3 ${
-                                pathname == "/settings"
-                                    ? "bg-palette4"
-                                    : "hover:bg-palette4"
-                            }`}
-                        >
-                            <Cog className="w-10 h-10" />
-                            <span className="font-semibold text-2xl">
-                                Setting
-                            </span>
-                        </Link>
-                    </div>
-                </div>
-            </nav>
-            {/* overlay */}
-            {isSidebarOpen && (
-                <div
-                    className={`fixed inset-0 bg-black z-10 bg-opacity-50 transition-opacity duration-300 ease-in-out ${
-                        isSidebarOpen
-                            ? "opacity-100 pointer-events-auto"
-                            : "opacity-0 pointer-events-none"
-                    }`}
-                    onClick={toggleSidebar}
-                />
-            )}
+
+            {/* sidebar component */}
+            <Sidebar
+                isSidebarOpen={isSidebarOpen}
+                toggleSidebar={toggleSidebar}
+            />
         </>
     );
 };
