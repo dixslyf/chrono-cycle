1f71cce4a3c6d2931ecf7dd30997a57e02b23e62
keith <keithneo00@gmail.com>
2025-02-24 22:46:55 +0800

    feat(components/settings/): created new component folder This settings folder will hold all the settings component. Similarly to other component folders it will contain a bunch of components with a main component file `settingsForm.tsx` which will be the one being imported in the settings page.
    refactor(settings/): Added the settings component to the page

---
+++ chrono-cycle/app/components/settings/settingsForm.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import { useState } from "react";
+
+const SettingsForm = () => {
+    const [dateFormat, setDataFormat] = useState<string>("MM/dd/yyyy");
+    const daysOfWeek: string[] = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+
+    return (
+        <>
+            <form action="">
+                {/* general */}
+                <section>
+                    {/* title */}
+                    <div>
+                        <h1>General</h1>
+                        <span>Manage general settings</span>
+                    </div>
+                    {/* day of week */}
+                    <div>
+                        <label htmlFor="day">Start Day of Week</label>
+                        <select name="day" id="day">
+                            {daysOfWeek.map((day, index) => (
+                                <option value={day} key={index}>
+                                    {day}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+
+                    {/* date format */}
+                    <div>
+                        <label htmlFor="dateFormat">Date Format</label>
+                        <input
+                            type="text"
+                            name="dataFormat"
+                            id="dataFormat"
+                            value={dateFormat}
+                            onChange={(e) => setDataFormat(e.target.value)}
+                        />
+                    </div>
+                    <hr />
+                </section>
+
+                {/* notification */}
+                <section>
+                    {/* title */}
+                    <div>
+                        <h1>Notification</h1>
+                        <span>update your notification configurations</span>
+                    </div>
+
+                    {/* email notifications */}
+                    <div>
+                        <label htmlFor="emailToggle">Email Notifications</label>
+                        {/* TODO */}
+                        {/* Create toggle */}
+                        <input
+                            type="checkbox"
+                            name="emailToggle"
+                            id="emailToggle"
+                        />
+                        <span></span>
+                    </div>
+
+                    {/* desktop notifications */}
+                    <div>
+                        <label htmlFor="desktopToggle">
+                            Desktop Notifications
+                        </label>
+                        {/* TODO */}
+                        {/* Create toggle */}
+                        <input
+                            type="checkbox"
+                            name="desktopToggle"
+                            id="desktopToggle"
+                        />
+                        <span></span>
+                    </div>
+                    <hr />
+                </section>
+
+                {/* submit button */}
+                <section>
+                    {/* TODO */}
+                    {/* Should have this entire section show up only when there are changes to any inputs */}
+                    <button type="submit">Save Changes</button>
+                </section>
+            </form>
+        </>
+    );
+};
+
+export default SettingsForm;
