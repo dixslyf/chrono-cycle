35231d4766e089fe55c4d59a746858a9b381805a
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2025-02-28 00:34:17 +0800

    feat(event-templates): add ability to list event templates for a project template

---
+++ chrono-cycle/server/event-templates/list/data.ts
@@ -0,0 +1,25 @@
+import * as E from "fp-ts/Either";
+import { z } from "zod";
+
+import {
+    DoesNotExistError,
+    InternalError,
+    ValidationError,
+} from "@/server/common/errors";
+import { EventTemplate } from "@/server/event-templates/common/data";
+import { encodedIdSchema } from "@/server/common/identifiers";
+
+export const listFormDataSchema = z.object({
+    projectTemplateId: encodedIdSchema,
+});
+
+export type ListFormData = z.output<typeof listFormDataSchema>;
+
+export type ListReturnData = EventTemplate[];
+
+export type ListError =
+    | ValidationError<"projectTemplateId">
+    | DoesNotExistError
+    | InternalError;
+
+export type ListResult = E.Either<ListError, ListReturnData>;
