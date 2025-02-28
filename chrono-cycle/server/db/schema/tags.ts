8c44df950d830b473abb5edf7504bdd2a4a5c36f
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2025-02-28 21:54:35 +0800

    refactor(db): include user ID foreign key in tags table
    Allows for easy querying of a user's tags.

--- chrono-cycle/server/db/schema/tags.ts
+++ chrono-cycle/server/db/schema/tags.ts
@@ -1,15 +1,23 @@
 import { InferSelectModel, InferInsertModel } from "drizzle-orm";
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { integer, pgTable, serial, text, unique } from "drizzle-orm/pg-core";
 import {
     createInsertSchema,
     createSelectSchema,
     createUpdateSchema,
 } from "drizzle-zod";
+import users from "./users";
 
-export const tags = pgTable("tags", {
-    id: serial("id").primaryKey().unique(),
-    name: text("name").notNull(),
-});
+export const tags = pgTable(
+    "tags",
+    {
+        id: serial("id").primaryKey().unique(),
+        userId: integer("user_id")
+            .notNull()
+            .references(() => users.id),
+        name: text("name").notNull(),
+    },
+    (t) => [unique("tags_unique_user_id_name").on(t.userId, t.name)],
+);
 
 export type DbTag = InferSelectModel<typeof tags>;
 export type DbTagInsert = InferInsertModel<typeof tags>;
