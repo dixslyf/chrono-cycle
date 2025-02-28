cf30d8d24fd00deec9f481d9079e2883f28e0f9d
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2025-02-28 18:57:02 +0800

    refactor(db): bikeshed

--- chrono-cycle/server/db/schema/userSettings.ts
+++ chrono-cycle/server/db/schema/userSettings.ts
@@ -2,36 +2,32 @@ import { InferSelectModel } from "drizzle-orm";
 import { pgEnum, pgTable, integer, boolean } from "drizzle-orm/pg-core";
 import users from "./users";
 
-// Define the enum for startDayOfWeek
 export const startDayOfWeekEnum = pgEnum("start_day_of_week", [
     "Monday",
     "Sunday",
 ]);
 
-// Define the enum for dateFormat
 export const dateFormatEnum = pgEnum("date_format", [
     "MM/DD/YYYY",
     "DD/MM/YYYY",
     "YYYY/MM/DD",
 ]);
 
-export const settings = pgTable("settings", {
+export const userSettings = pgTable("user_settings", {
     userId: integer("user_id")
         .references(() => users.id)
         .primaryKey()
         .notNull(),
-
     startDayOfWeek: startDayOfWeekEnum("start_day_of_week").notNull(),
-
     dateFormat: dateFormatEnum("date_format").notNull(),
-
-    emailNotification: boolean("email_notification").notNull().default(false),
-
-    desktopNotification: boolean("desktop_notification")
+    enableEmailNotifications: boolean("enable_email_notifications")
+        .notNull()
+        .default(false),
+    enableDesktopNotifications: boolean("enable_desktop_notifications")
         .notNull()
         .default(false),
 });
 
-export type Setting = InferSelectModel<typeof settings>;
+export type UserSettings = InferSelectModel<typeof userSettings>;
 
-export default settings;
+export default userSettings;
