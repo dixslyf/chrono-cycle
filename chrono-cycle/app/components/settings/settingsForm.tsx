de0366a7e22642a9cb8bd00fc0c9a880236e826d
khonminthite <kyukyu2112017@gmail.com>
2025-02-28 15:09:04 +0800

    settingschema

--- chrono-cycle/app/components/settings/settingsForm.tsx
+++ chrono-cycle/app/components/settings/settingsForm.tsx
@@ -1,14 +1,9 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
-const SettingsForm = ({ userId, router }: { userId: number; router: any }) => {
-    const [dateFormat, setDateFormat] = useState<string>("MM/dd/yyyy");
-    const [startDayOfWeek, setStartDayOfWeek] = useState<string>("Sunday");
-    const [emailNotifications, setEmailNotifications] = useState<boolean>(false);
-    const [desktopNotifications, setDesktopNotifications] = useState<boolean>(false);
-    const [isLoading, setIsLoading] = useState<boolean>(true);
-
+const SettingsForm = () => {
+    const [dateFormat, setDataFormat] = useState<string>("MM/dd/yyyy");
     const daysOfWeek: string[] = [
         "Sunday",
         "Monday",
@@ -19,133 +14,88 @@ const SettingsForm = ({ userId, router }: { userId: number; router: any }) => {
         "Saturday",
     ];
 
-    // Fetch user settings
-    useEffect(() => {
-        const fetchUserSettings = async () => {
-            try {
-                const response = await fetch(`./settingshandler/userId=${userId}`);
-                if (!response.ok) {
-                    throw new Error("Failed to fetch user settings");
-                }
-                const data = await response.json();
-                setStartDayOfWeek(data.startDayOfWeek);
-                setDateFormat(data.dateFormat);
-                setEmailNotifications(data.emailNotification);
-                setDesktopNotifications(data.desktopNotification);
-            } catch (error) {
-                console.error("Error fetching user settings:", error);
-            } finally {
-                setIsLoading(false);
-            }
-        };
-
-        fetchUserSettings();
-    }, [userId]);
-
-    const handleSubmit = async (event: React.FormEvent) => {
-        event.preventDefault();
-
-        // Update user settings
-        try {
-            const response = await fetch(`./settingshandler/userId=${userId}`, {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    startDayOfWeek,
-                    dateFormat,
-                    emailNotification: emailNotifications,
-                    desktopNotification: desktopNotifications,
-                }),
-            });
-
-            if (!response.ok) {
-                throw new Error("Failed to save user settings");
-            }
-
-            // Redirect to a success page or show a success message
-            router.push("/settings/success");
-        } catch (error) {
-            console.error("Error saving settings:", error);
-        }
-    };
-
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
-
     return (
-        <form onSubmit={handleSubmit}>
-            <section>
-                <div>
-                    <h1>General</h1>
-                    <span>Manage general settings</span>
-                </div>
+        <>
+            <form action="">
+                {/* general */}
+                <section>
+                    {/* title */}
+                    <div>
+                        <h1>General</h1>
+                        <span>Manage general settings</span>
+                    </div>
+                    {/* day of week */}
+                    <div>
+                        <label htmlFor="day">Start Day of Week</label>
+                        <select name="day" id="day">
+                            {daysOfWeek.map((day, index) => (
+                                <option value={day} key={index}>
+                                    {day}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
 
-                <div>
-                    <label htmlFor="day">Start Day of Week</label>
-                    <select
-                        name="day"
-                        id="day"
-                        value={startDayOfWeek}
-                        onChange={(e) => setStartDayOfWeek(e.target.value)}
-                    >
-                        {daysOfWeek.map((day, index) => (
-                            <option value={day} key={index}>
-                                {day}
-                            </option>
-                        ))}
-                    </select>
-                </div>
+                    {/* date format */}
+                    <div>
+                        <label htmlFor="dateFormat">Date Format</label>
+                        <input
+                            type="text"
+                            name="dataFormat"
+                            id="dataFormat"
+                            value={dateFormat}
+                            onChange={(e) => setDataFormat(e.target.value)}
+                        />
+                    </div>
+                    <hr />
+                </section>
 
-                <div>
-                    <label htmlFor="dateFormat">Date Format</label>
-                    <input
-                        type="text"
-                        name="dateFormat"
-                        id="dateFormat"
-                        value={dateFormat}
-                        onChange={(e) => setDateFormat(e.target.value)}
-                    />
-                </div>
-                <hr />
-            </section>
+                {/* notification */}
+                <section>
+                    {/* title */}
+                    <div>
+                        <h1>Notifications</h1>
+                        <span>Update your notification preferences</span>
+                    </div>
 
-            <section>
-                <div>
-                    <h1>Notifications</h1>
-                    <span>Update your notification preferences</span>
-                </div>
+                    {/* email notifications */}
+                    <div>
+                        <label htmlFor="emailToggle">Email Notifications</label>
+                        {/* TODO */}
+                        {/* Create toggle */}
+                        <input
+                            type="checkbox"
+                            name="emailToggle"
+                            id="emailToggle"
+                        />
+                        <span></span>
+                    </div>
 
-                <div>
-                    <label htmlFor="emailToggle">Email Notifications</label>
-                    <input
-                        type="checkbox"
-                        name="emailToggle"
-                        id="emailToggle"
-                        checked={emailNotifications}
-                        onChange={(e) => setEmailNotifications(e.target.checked)}
-                    />
-                </div>
+                    {/* desktop notifications */}
+                    <div>
+                        <label htmlFor="desktopToggle">
+                            Desktop Notifications
+                        </label>
+                        {/* TODO */}
+                        {/* Create toggle */}
+                        <input
+                            type="checkbox"
+                            name="desktopToggle"
+                            id="desktopToggle"
+                        />
+                        <span></span>
+                    </div>
+                    <hr />
+                </section>
 
-                <div>
-                    <label htmlFor="desktopToggle">Desktop Notifications</label>
-                    <input
-                        type="checkbox"
-                        name="desktopToggle"
-                        id="desktopToggle"
-                        checked={desktopNotifications}
-                        onChange={(e) => setDesktopNotifications(e.target.checked)}
-                    />
-                </div>
-                <hr />
-            </section>
-
-            <section>
-                <button type="submit">Save Changes</button>
-            </section>
-        </form>
+                {/* submit button */}
+                <section>
+                    {/* TODO */}
+                    {/* Should have this entire section show up only when there are changes to any inputs */}
+                    <button type="submit">Save Changes</button>
+                </section>
+            </form>
+        </>
     );
 };
 
