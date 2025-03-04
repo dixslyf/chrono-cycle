f46a15020a207031946a8b16565baa0533c6b423
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2025-03-04 20:19:02 +0800

    feat(ui/templates): add mantine-based button and form for project template creation

---
+++ chrono-cycle/app/components/templates/createTemplateButton.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Button, Modal } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { Plus } from "lucide-react";
+import { CreateProjectTemplateForm } from "./createTemplateForm";
+
+export function CreateProjectTemplateButton() {
+    const [opened, { open, close }] = useDisclosure(false);
+    return (
+        <>
+            <Modal
+                opened={opened}
+                onClose={close}
+                title="Create Project Template"
+                centered
+            >
+                <CreateProjectTemplateForm onSuccess={close} />
+            </Modal>
+            <Button variant="filled" onClick={open}>
+                <Plus />
+            </Button>
+        </>
+    );
+}
