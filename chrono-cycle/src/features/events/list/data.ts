e76685818dff9c4e59a343d3d73d58162173be37
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2025-03-15 12:42:31 +0800

    feat(features/events): add server action for listing events for a project

---
+++ chrono-cycle/src/features/events/list/data.ts
@@ -0,0 +1,25 @@
+import * as E from "fp-ts/Either";
+import { z } from "zod";
+
+import { Event } from "@/common/data/domain";
+import {
+    DoesNotExistError,
+    InternalError,
+    ValidationError,
+} from "@/common/errors";
+
+import { encodedIdSchema } from "@/lib/identifiers";
+
+export const payloadSchema = z.object({
+    projectId: encodedIdSchema,
+});
+
+export type Payload = z.input<typeof payloadSchema>;
+export type ParsedPayload = z.output<typeof payloadSchema>;
+
+export type Failure =
+    | ValidationError<"projectId">
+    | DoesNotExistError
+    | InternalError;
+
+export type Result = E.Either<Failure, Event[]>;
