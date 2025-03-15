b376edcfaa54496b71059c6b414125626690a431
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2025-03-15 12:16:16 +0800

    feat(features/event-templates): add server action for updating

---
+++ chrono-cycle/src/features/event-templates/update/bridge.ts
@@ -0,0 +1,48 @@
+import { pipe } from "fp-ts/function";
+import * as TE from "fp-ts/TaskEither";
+
+import { EventTemplate, toEventTemplate } from "@/common/data/domain";
+import { AssertionError, DoesNotExistError } from "@/common/errors";
+
+import {
+    decodeProjectTemplateId,
+    decodeReminderTemplateId,
+} from "@/lib/identifiers";
+
+import getDb from "@/db";
+import { updateEventTemplate } from "@/db/queries/event-templates/update";
+
+import { ParsedPayload } from "./data";
+
+export function bridge(
+    userId: number,
+    payloadP: ParsedPayload,
+): TE.TaskEither<DoesNotExistError | AssertionError, EventTemplate> {
+    return pipe(
+        TE.fromTask(getDb),
+        TE.chain((db) => {
+            const { id, remindersDelete, remindersUpdate, tags, ...rest } =
+                payloadP;
+
+            return updateEventTemplate(db, userId, {
+                id: decodeProjectTemplateId(id),
+                remindersDelete: remindersDelete.map((id) =>
+                    decodeReminderTemplateId(id),
+                ),
+                remindersUpdate: remindersUpdate.map((rt) => {
+                    const { id, ...rest } = rt;
+                    return {
+                        id: decodeReminderTemplateId(id),
+                        ...rest,
+                    };
+                }),
+                tags: tags.map((name) => ({
+                    userId,
+                    name,
+                })),
+                ...rest,
+            });
+        }),
+        TE.map(toEventTemplate),
+    );
+}
