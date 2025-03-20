9a15fdb814c7d9ecb29a442bb1e2ff35d6f77e78
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2025-03-20 21:21:40 +0800

    feat(lib/reminders): add helper functions to schedule a reminder

--- chrono-cycle/src/lib/reminders.ts
+++ chrono-cycle/src/lib/reminders.ts
@@ -1,6 +1,19 @@
+import {
+    EmailReminderRunHandle,
+    emailReminderTask,
+} from "@/trigger/emailReminder";
+import { runs, tasks } from "@trigger.dev/sdk/v3";
+import * as A from "fp-ts/Array";
 import * as E from "fp-ts/Either";
+import { identity, pipe } from "fp-ts/function";
+import * as T from "fp-ts/Task";
+import * as TE from "fp-ts/TaskEither";
 
-import { MalformedTimeStringError } from "@/common/errors";
+import { Project, Reminder } from "@/common/data/domain";
+import {
+    MalformedTimeStringError,
+    ScheduleReminderError,
+} from "@/common/errors";
 
 export type TimeComponents = {
     hours: number;
@@ -23,4 +36,51 @@ export function extractTimeStringComponents(
     }
 
     return E.right({ hours, minutes });
+}
+
+export function scheduleReminder(
+    reminder: Reminder,
+): TE.TaskEither<ScheduleReminderError, EmailReminderRunHandle> {
+    return TE.tryCatch(
+        () =>
+            tasks.trigger<typeof emailReminderTask>("email-reminder", {
+                reminderId: reminder.id,
+                eventId: reminder.eventId,
+                triggerTime: reminder.triggerTime,
+            }),
+        (err) =>
+            ScheduleReminderError(
+                reminder.id,
+                err instanceof Error ? err.message : undefined,
+            ),
+    );
+}
+
+export function scheduleReminders(
+    project: Project,
+): TE.TaskEither<ScheduleReminderError[], EmailReminderRunHandle[]> {
+    return pipe(
+        // Try scheduling the reminders.
+        project.events
+            .map((event) => event.reminders)
+            .flat()
+            .filter((reminder) => reminder.emailNotifications)
+            .map((reminder) => scheduleReminder(reminder)),
+        A.sequence(T.ApplicativePar), // Convert to Task<Either<ScheduleReminderError, EmailReminderRunhandle>[]>
+        T.map(A.partitionMap(identity)), // Separate the failures from the successes
+        TE.fromTask,
+        // Check for failures. If there is a failure, then cancel the successful ones
+        // and return the errors.
+        TE.chain(({ left: failures, right: handles }) =>
+            failures.length > 0
+                ? pipe(
+                      handles,
+                      A.traverse(T.ApplicativePar)(
+                          (handle) => () => runs.cancel(handle.id),
+                      ),
+                      () => TE.left(failures),
+                  )
+                : TE.right(handles),
+        ),
+    );
 }
