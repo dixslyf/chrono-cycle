0f04e18b63a7b69c3e4902f38ecccd6b416ca8c0
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2025-03-21 12:59:56 +0800

    refactor(lib/reminders): add cancel reminder function and split scheduling of reminders

--- chrono-cycle/src/lib/reminders.ts
+++ chrono-cycle/src/lib/reminders.ts
@@ -9,12 +9,16 @@ import { identity, pipe } from "fp-ts/function";
 import * as T from "fp-ts/Task";
 import * as TE from "fp-ts/TaskEither";
 
-import { Project, Reminder } from "@/common/data/domain";
 import {
+    CancelReminderError,
     MalformedTimeStringError,
     ScheduleReminderError,
     ScheduleReminderIssue,
 } from "@/common/errors";
+
+import { DbExpandedProject, DbReminder } from "@/db/schema";
+
+import { encodeEventId, encodeReminderId } from "./identifiers";
 
 export type TimeComponents = {
     hours: number;
@@ -39,32 +43,53 @@ export function extractTimeStringComponents(
     return E.right({ hours, minutes });
 }
 
-export function scheduleReminder(
-    reminder: Reminder,
-): TE.TaskEither<ScheduleReminderIssue, EmailReminderRunHandle> {
+export type ScheduledDbReminder = {
+    reminder: DbReminder;
+    handle: EmailReminderRunHandle;
+};
+
+export function cancelReminder(
+    runId: string,
+): TE.TaskEither<CancelReminderError, void> {
     return TE.tryCatch(
-        () =>
-            tasks.trigger<typeof emailReminderTask>("email-reminder", {
-                reminderId: reminder.id,
-                eventId: reminder.eventId,
-                triggerTime: reminder.triggerTime,
-            }),
+        () => runs.cancel(runId).then(() => undefined),
         (err) =>
-            ({
-                reminderId: reminder.id,
-                context: err instanceof Error ? err.message : undefined,
-            }) satisfies ScheduleReminderIssue,
+            CancelReminderError(
+                runId,
+                err instanceof Error ? err.message : undefined,
+            ),
+    );
+}
+
+export function scheduleReminder(
+    dbReminder: DbReminder,
+): TE.TaskEither<ScheduleReminderIssue, ScheduledDbReminder> {
+    return pipe(
+        TE.tryCatch(
+            () =>
+                tasks.trigger<typeof emailReminderTask>("email-reminder", {
+                    reminderId: encodeReminderId(dbReminder.id),
+                    eventId: encodeEventId(dbReminder.eventId),
+                    triggerTime: dbReminder.triggerTime,
+                }),
+            (err) =>
+                ({
+                    reminderId: encodeReminderId(dbReminder.id),
+                    context: err instanceof Error ? err.message : undefined,
+                }) satisfies ScheduleReminderIssue,
+        ),
+        TE.map((handle) => ({ reminder: dbReminder, handle })),
     );
 }
 
 export function scheduleReminders(
-    project: Project,
-): TE.TaskEither<ScheduleReminderError, EmailReminderRunHandle[]> {
+    reminders: DbReminder[],
+): TE.TaskEither<
+    ScheduleReminderError | CancelReminderError,
+    ScheduledDbReminder[]
+> {
     return pipe(
-        // Try scheduling the reminders.
-        project.events
-            .map((event) => event.reminders)
-            .flat()
+        reminders
             // Filter to those with email notifications and whose trigger is later
             // the current date and time.
             .filter(
@@ -72,22 +97,35 @@ export function scheduleReminders(
                     reminder.emailNotifications &&
                     reminder.triggerTime >= new Date(),
             )
+            // Try scheduling the reminders.
             .map((reminder) => scheduleReminder(reminder)),
         A.sequence(T.ApplicativePar), // Convert to Task<Either<ScheduleReminderError, EmailReminderRunhandle>[]>
         T.map(A.partitionMap(identity)), // Separate the failures from the successes
         TE.fromTask,
         // Check for failures. If there is a failure, then cancel the successful ones
         // and return the errors.
-        TE.chain(({ left: issues, right: handles }) =>
+        TE.chain(({ left: issues, right: scheduledReminders }) =>
             issues.length > 0
                 ? pipe(
-                      handles,
-                      A.traverse(T.ApplicativePar)(
-                          (handle) => () => runs.cancel(handle.id),
+                      scheduledReminders,
+                      A.traverse(TE.ApplicativePar)((scheduledReminder) =>
+                          cancelReminder(scheduledReminder.handle.id),
                       ),
                       () => TE.left(ScheduleReminderError(issues)),
                   )
-                : TE.right(handles),
+                : TE.right(scheduledReminders),
         ),
     );
+}
+
+export function scheduleRemindersForProject(
+    dbExpandedProject: DbExpandedProject,
+): TE.TaskEither<
+    ScheduleReminderError | CancelReminderError,
+    ScheduledDbReminder[]
+> {
+    const dbReminders = dbExpandedProject.events
+        .map((dbEvent) => dbEvent.reminders)
+        .flat();
+    return scheduleReminders(dbReminders);
 }
