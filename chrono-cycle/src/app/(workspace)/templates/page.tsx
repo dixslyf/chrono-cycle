796561c9b249737c0e7e71f5db4a64a6eeeb7598
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2025-03-23 00:10:26 +0800

    refactor(ui/templateTable): use tanstack query
    So that we can share the results later with the create project form.

--- chrono-cycle/src/app/(workspace)/templates/page.tsx
+++ chrono-cycle/src/app/(workspace)/templates/page.tsx
@@ -1,23 +1,11 @@
 // template page
 
 import { Group, Stack } from "@mantine/core";
-import * as E from "fp-ts/Either";
-import { pipe } from "fp-ts/function";
 
 import { CreateProjectTemplateButton } from "@/app/components/templates/createProjectTemplateButton";
 import { ProjectTemplatesTable } from "@/app/components/templates/projectTemplatesTable";
 
-import { ProjectTemplateOverview } from "@/common/data/domain";
-
-import { listProjectTemplatesAction } from "@/features/project-templates/list/action";
-
 export default async function Templates() {
-    const listProjectTemplatesResult = await listProjectTemplatesAction();
-    const entries = pipe(
-        listProjectTemplatesResult,
-        E.getOrElse(() => [] as ProjectTemplateOverview[]),
-    );
-
     return (
         <>
             {/* <h1>This is the template page</h1> */}
@@ -29,7 +17,7 @@ export default async function Templates() {
             {/* create template section */}
             <section className="w-full flex justify-center">
                 <Stack>
-                    <ProjectTemplatesTable entries={entries} />
+                    <ProjectTemplatesTable />
                     <Group justify="flex-end">
                         <CreateProjectTemplateButton />
                     </Group>
