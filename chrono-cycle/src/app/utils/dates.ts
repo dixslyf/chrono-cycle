f7ec2163e0359ca81a010d24588fcc5f11ce6098
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2025-03-23 15:43:30 +0800

    feat(ui/utils): add formatting functions for dates

--- chrono-cycle/src/app/utils/dates.ts
+++ chrono-cycle/src/app/utils/dates.ts
@@ -1,3 +1,5 @@
+import dayjs from "dayjs";
+
 import { Day } from "@/app/components/dashboard/timeline";
 
 export function generateDaysInRange(startDate: Date, endDate: Date): Day[] {
@@ -16,4 +18,26 @@ export function generateDaysInRange(startDate: Date, endDate: Date): Day[] {
 
 export function areSameDay(a: Date, b: Date): boolean {
     return a.toDateString() === b.toDateString();
+}
+
+export function formatDate(
+    date: Date,
+    config?: { withTime?: boolean },
+): string {
+    const dateFormat = "dddd, MMMM D, YYYY";
+    const timeFormat = "h:mm A";
+    const format = config?.withTime
+        ? `${timeFormat} [on] ${dateFormat}`
+        : dateFormat;
+    return dayjs(date).format(format);
+}
+
+export function formatReminderTemplateTime(time: string) {
+    // Match "HH:MM".
+    const match = time.match(/^(\d{2}):(\d{2})/);
+
+    // Warning: Assumes the string is well-formed.
+    // First value is the entire matched part.
+    const [_, hours, minutes] = match!;
+    return `${hours}:${minutes}`;
 }
