025137490cc6d7243223b434c5f1e04c5b80d7ea
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2025-03-24 18:03:00 +0800

    feat(ui/deleteEventTemplateButton): init

---
+++ chrono-cycle/src/app/components/templates/deleteEventTemplateButton.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { Button } from "@mantine/core";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import * as E from "fp-ts/Either";
+import { pipe } from "fp-ts/function";
+import { Trash } from "lucide-react";
+
+import { notifyError, notifySuccess } from "@/app/utils/notifications";
+
+import { deleteEventTemplatesAction } from "@/features/event-templates/delete/action";
+import { Failure } from "@/features/event-templates/delete/data";
+
+export function DeleteEventTemplateButton({
+    eventTemplateId,
+    onSuccess,
+    disabled,
+}: {
+    eventTemplateId: string;
+    onSuccess: () => void;
+    disabled?: boolean;
+}): React.ReactNode {
+    const queryClient = useQueryClient();
+    const deleteMutation = useMutation({
+        mutationFn: async (eventTemplateId: string) => {
+            const result = await deleteEventTemplatesAction({
+                eventTemplateIds: [eventTemplateId],
+            });
+            return pipe(
+                result,
+                E.getOrElseW((err) => {
+                    throw err;
+                }),
+            );
+        },
+        onError: (_err: Failure) => {
+            notifyError({
+                message: "Failed to delete event template.",
+            });
+        },
+        onSuccess: () => {
+            notifySuccess({
+                message: "Successfully deleted event template.",
+            });
+            queryClient.invalidateQueries({
+                queryKey: ["retrieve-project-template-data"],
+            });
+            onSuccess();
+        },
+    });
+
+    return (
+        <Button
+            variant="filled"
+            color="red"
+            disabled={deleteMutation.isPending || disabled}
+            loading={deleteMutation.isPending}
+            onClick={() => deleteMutation.mutate(eventTemplateId)}
+        >
+            <Trash className="mr-2" />
+            Delete
+        </Button>
+    );
+}
