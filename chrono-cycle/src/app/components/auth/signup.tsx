624e37fd84f207900572473e84c481fbe65dc2b3
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2025-03-26 16:53:41 +0800

    refactor(ui/signup): use notifications and set form errors based on server return value

--- chrono-cycle/src/app/components/auth/signup.tsx
+++ chrono-cycle/src/app/components/auth/signup.tsx
@@ -1,39 +1,35 @@
 // sign up component
 "use client";
 
-import { TextInput } from "@mantine/core";
+import { Button, TextInput } from "@mantine/core";
 import { useForm, zodResolver } from "@mantine/form";
+import { useMutation } from "@tanstack/react-query";
 import * as E from "fp-ts/Either";
+import { pipe } from "fp-ts/lib/function";
 import { startTransition, useActionState } from "react";
-import { match } from "ts-pattern";
+import { match, P } from "ts-pattern";
+
+import { notifyError, notifySuccess } from "@/app/utils/notifications";
 
 import { signUpAction } from "@/features/auth/signUp/action";
-import { payloadSchema, Result } from "@/features/auth/signUp/data";
+import { Failure, payloadSchema, Result } from "@/features/auth/signUp/data";
 
-function getErrorMessage(formState: Result): string {
-    return match(formState)
-        .with(
-            { left: { _errorKind: "ValidationError" } },
-            () => "Invalid or missing fields",
-        )
-        .with(
-            { left: { _errorKind: "DuplicateNameError" } },
-            () => "Username or email address is already taken",
-        )
-        .with(
-            { left: { _errorKind: "InternalError" } },
-            () => "An internal error occurred",
-        )
-        .otherwise(() => "");
+function extractFormIssues(failure: Failure): {
+    username?: string;
+    email?: string;
+    password?: string;
+} {
+    return match(failure)
+        .with({ _errorKind: "UsernameTakenError" }, () => ({
+            username: "Username is already taken",
+        }))
+        .with({ _errorKind: "EmailTakenError" }, () => ({
+            email: "Email address is already registered",
+        }))
+        .otherwise(() => ({}));
 }
 
 const SignupForm = () => {
-    // Server-side action for form submission.
-    const [formState, formAction, isPending] = useActionState(
-        signUpAction,
-        null,
-    );
-
     const form = useForm({
         mode: "controlled",
         validateInputOnChange: true,
@@ -44,6 +40,33 @@ const SignupForm = () => {
         },
         validate: zodResolver(payloadSchema),
     });
+    type FormValues = typeof form.values;
+
+    // Server-side action for form submission.
+    const signUpMutation = useMutation({
+        mutationFn: async (values: FormValues) => {
+            const result = await signUpAction(null, values);
+            return pipe(
+                result,
+                E.getOrElseW((err) => {
+                    throw err;
+                }),
+            );
+        },
+        onSuccess: () =>
+            notifySuccess({
+                message:
+                    "Your account has been successfully created! Please sign in.",
+            }),
+        onError: (failure: Failure) => {
+            const formIssues = extractFormIssues(failure);
+            form.setErrors(formIssues);
+            console.log(formIssues);
+            if (Object.keys(formIssues).length === 0) {
+                notifyError({ message: "An internal error occurred." });
+            }
+        },
+    });
 
     return (
         <div className="w-full h-full flex flex-col gap-10">
@@ -51,22 +74,9 @@ const SignupForm = () => {
             <h1 className="text-palette1 font-bold text-3xl p-5">Sign Up</h1>
             <form
                 onSubmit={form.onSubmit((values) =>
-                    startTransition(() => formAction(values)),
+                    signUpMutation.mutate(values),
                 )}
             >
-                {/* error or success message */}
-                <div className="w-full flex justify-center mb-4 font-semibold">
-                    {formState &&
-                        (E.isRight(formState) ? (
-                            <p className={"text-green-500 text-lg"}>
-                                Account successfully created! Please sign in.
-                            </p>
-                        ) : (
-                            <p className={"text-red-500 text-lg"}>
-                                {getErrorMessage(formState)}
-                            </p>
-                        ))}
-                </div>
                 <div className="flex flex-col items-center gap-16">
                     <div className="w-full flex flex-col items-center gap-5">
                         {/* username input */}
@@ -121,19 +131,14 @@ const SignupForm = () => {
                             {...form.getInputProps("password")}
                         />
                     </div>
-
-                    {/* button */}
-                    <button
+                    <Button
                         type="submit"
-                        disabled={
-                            // TODO: Disable while submitting and if there are validation errors.
-                            isPending
-                        }
+                        disabled={Object.keys(form.errors).length > 0}
+                        loading={signUpMutation.isPending}
                         className="w-3/4 p-1 rounded-xl bg-palette2 hover:bg-[#a08368] transition duration-300 text-palette3"
                     >
-                        {/* Sign Up */}
-                        {isPending ? "Signing Up..." : "Sign Up"}
-                    </button>
+                        Sign Up
+                    </Button>
                 </div>
             </form>
         </div>
