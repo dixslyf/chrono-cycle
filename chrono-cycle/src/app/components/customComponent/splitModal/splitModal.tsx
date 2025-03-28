c89f93e95faa55c6f7c5d893442838567717e7a6
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2025-03-28 15:47:22 +0800

    refactor(ui/splitModal): allow specifying title in left and right sections

--- chrono-cycle/src/app/components/customComponent/splitModal/splitModal.tsx
+++ chrono-cycle/src/app/components/customComponent/splitModal/splitModal.tsx
@@ -1,12 +1,20 @@
 "use client";
 
-import { Box, Group, Modal, Stack, type ModalProps } from "@mantine/core";
+import {
+    Box,
+    Group,
+    Modal,
+    Stack,
+    StackProps,
+    Text,
+    type ModalProps,
+} from "@mantine/core";
 import { ReactNode } from "react";
 
 import classes from "./split-modal.module.css";
 
 const SECTION_PX = 30;
-const SECTION_PY = 22;
+const SECTION_PY = 24;
 
 export type SplitModalProps = Omit<ModalProps, "withCloseButton" | "title">;
 
@@ -28,27 +36,58 @@ export function SplitModal(props: SplitModalProps): ReactNode {
     );
 }
 
-function SplitModalLeft(props: { title?: string; children: ReactNode }) {
+function SplitModalLeft(props: {
+    title?: string;
+    stackProps?: StackProps;
+    flexRatio?: number;
+    children: ReactNode;
+}) {
     return (
-        <Box px={SECTION_PX} py={SECTION_PY} style={{ flex: 2 }}>
-            {props.children}
-        </Box>
+        <Stack
+            gap="xl"
+            px={SECTION_PX}
+            py={SECTION_PY}
+            style={{ flex: props.flexRatio ?? 5 }}
+            {...props.stackProps}
+        >
+            <Text className="text-3xl font-bold h-1/8">{props.title}</Text>
+            <Box h="100%" w="100%">
+                {props.children}
+            </Box>
+        </Stack>
     );
 }
 
-function SplitModalRight(props: { children: ReactNode }) {
+function SplitModalRight(props: {
+    title?: string;
+    stackProps?: StackProps;
+    flexRatio?: number;
+    children: ReactNode;
+}) {
     return (
-        <Stack gap={0} className="bg-palette1">
-            <Group justify="flex-end" px={20} py={16} style={{ flex: 1 }}>
-                <Modal.CloseButton
-                    iconSize={30}
-                    style={{
-                        color: "var(--mantine-color-white)",
-                    }}
-                    className={classes.closeButton}
-                />
+        <Stack
+            gap="xl"
+            style={{ flex: props.flexRatio ?? 3 }}
+            className="bg-palette1"
+            {...props.stackProps}
+        >
+            <Group justify="flex-end" align="flex-start">
+                <Box pl={SECTION_PX} pt={SECTION_PY} style={{ flex: 1 }}>
+                    <Text c="white" className="text-2xl font-bold h-1/8">
+                        {props.title}
+                    </Text>
+                </Box>
+                <Box pr={20} pt={16}>
+                    <Modal.CloseButton
+                        iconSize={30}
+                        style={{
+                            color: "var(--mantine-color-white)",
+                        }}
+                        className={classes.closeButton}
+                    />
+                </Box>
             </Group>
-            <Box h="100%" w="100%" px={SECTION_PX} py={SECTION_PY}>
+            <Box h="100%" w="100%" px={SECTION_PX} pb={SECTION_PY}>
                 {props.children}
             </Box>
         </Stack>
